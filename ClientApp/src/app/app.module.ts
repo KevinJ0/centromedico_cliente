36ea9ede436b93064441f10cdcd9e6e809c6d275
kevinj0 <kevinjooo59@gmail.com>
2021-06-19 20:58:05 -0400

    primera version del Home en angular agregado

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -5,28 +5,35 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
+import { HomeComponent } from './components/home/home.component';
+import { NbLayoutModule, NbThemeModule } from '@nebular/theme';
+import { FooterComponent } from './components/footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NewHomeComponent } from './components/home/new-home/new-home.component';
+import { BarnerComponent } from './barner/barner.component';
+
 
 @NgModule({
-  declarations: [
+  declarations: [ 
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    FooterComponent,
+    NewHomeComponent,
+    BarnerComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    
+    ], { useHash: false }),
+    NbLayoutModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
